84848df83c78b1d7a6bb20845f8d6b8778c9fde6
Paulo Junior <prakaji69@gmail.com>
2025-12-15 04:15:57 +0000

    Você está trabalhando em um sistema de gestão de condomínios. Existe um

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,26 @@
-
 /** @type {import('next').NextConfig} */
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
-
-    
+export default withPWA(nextConfig);
