2e44ad57b6c02f381fda173d3978c4c0b9fdd804
Mestre Anderson <treecommunity@treetechautomation.com>
2026-01-12 04:31:54 +0000

    usar outra porta (9002 é a melhor)
    Você já tem next dev rodando em 9002

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // This is required for Next.js to work with Firebase Studio.
+    // Isso previne erros de CORS no ambiente de desenvolvimento do Firebase Studio.
     allowedDevOrigins: [
-      'http://localhost:9000',
-      `https://9000-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
+      "http://localhost:9002",
+      "https://*.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev",
     ],
   },
 };
